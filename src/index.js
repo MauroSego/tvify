034da6acbc80785614e386c90bc52e9f827ef9f1
Maurosego <mauro.segovia.7@gmail.com>
2017-01-08 02:18:46 -0300

    Generado el index para la compilacion completa

--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@
 
 import $ from 'jquery'
 import page from 'page'
+import getShows from  'src/tvmaze-api-client' 
+import renderShows from 'src/render'
+import $tvShowsContainer from 'src/tv-shows-container'
 
 page('/', function(ctx, next){
-	next()
+  if(!localStorage.shows){
+  		getShows(function (shows){
+        $tvShowsContainer.find('.loader').remove();
+        localStorage.shows = JSON.stringify(shows);
+        renderShows(shows)
+  		})
+    } else {
+    renderShows(JSON.parse(localStorage.shows));
+  }
+  }
 });
 
 page();
