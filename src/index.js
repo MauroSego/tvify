6e7e7dc1cdc9b0b2e713c4be98c0cac87ede6e2f
Maurosego <mauro.segovia.7@gmail.com>
2017-01-08 02:10:56 -0300

    Creado render y tvshowcontainer

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+* Module dependencies
+**/
+
+import $ from 'jquery'
+import page from 'page'
+
+page('/', function(ctx, next){
+	next()
+});
+
+page();
--- src/tvmaze-api-client/index.js
+++ src/tvmaze-api-client/index.js
@@ -1,1 +1,23 @@
-index.js
+/**
+* Module dependencies
+**/
+
+import $ from 'jquery'
+
+export function getShows(fn) {
+	$.ajax('http://api.tvmaze.com/shows'), {
+		succes: function(shows, textStatus, xhr){
+			fn(shows)
+		}
+	}
+
+}
+
+export function searchShows(busqueda, fn){
+	$.ajax('http://api.tvmaze.com/search/shows', {
+		data: busqueda,
+		success: function(res, textStatus, xhr){
+			fn(res)
+		}
+	})
+}
